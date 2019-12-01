061a5fe19c410ee44c54e32115f04f4d007b5c23
dliu936 <54752285+dliu936@users.noreply.github.com>
2019-12-01 03:49:10 -0500

    Revamped report landing, added more reports
    Report dashboard is now landing page, added rpeorts with a conceputal view

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 web: python manage.py collectstatic --no-input; gunicorn crmapp.wsgi
+=======
+web: gunicorn csci_2019_project.config.wsgi --log-file -
+>>>>>>> Stashed changes
--- csci_2019_project/reports/urls.py
+++ csci_2019_project/reports/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from .views import ReportMainView, ReportExposureView, ReportDashView
+
+from .views import * # ReportExposureView, ReportDashView, ReportNavView, R
 
 #app_name = "reports"
 
 urlpatterns = [
-    path("", ReportMainView.as_view(), name="reports"),
+    path("", ReportDashView.as_view(), name="reports_dash"),
     path("dash", ReportDashView.as_view(), name="reports_dash"),
     path("exposure", ReportExposureView.as_view(), name="reports_exposure"),
+    path("volatility", ReportVolatilityView.as_view(), name="reports_volatility"),
+    path("financing", ReportFinancingView.as_view(), name="reports_financing"),
+    path("netassetvalue", ReportNavView.as_view(), name="reports_nav"),
 ]
