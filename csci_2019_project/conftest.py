a15b9405d74b23950383f83b62aecf1df6165da8
dliu936 <dli936@g.harvard.edu>
2019-12-16 12:31:33 -0500

    Fixes for Travis
    Fixes for Travis errors

--- csci_2019_project/conftest.py
+++ csci_2019_project/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from django.conf import settings
 from django.test import RequestFactory
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 from csci_2019_project.users.tests.factories import UserFactory
