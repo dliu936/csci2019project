8c1549a35e6cc8e8ef1c870d62c991467acecf0f
dliu936 <dli936@g.harvard.edu>
2019-12-16 13:45:39 -0500

    Fixes for Travis
    Fixes for Travis

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from django.test import RequestFactory
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
 
 
 from tests.factories import UserFactory
